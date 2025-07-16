9f29d224b88c5772f4ffd2cbd008464225637f04
juligric <49377944@est.ort.edu.ar>
2025-07-16 14:04:56 -0300

    .

--- script.js
+++ script.js
@@ -4,13 +4,9 @@ const rectangulo = document.querySelector('.rectangulo');
 window.addEventListener('scroll', function () {
   const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
-  // Desaparece si bajás más de 5px
   if (scrollTop > lastScrollTop + 5) {
     rectangulo.classList.add('oculto');
-  }
-
-  // Reaparece si subís más de 5px
-  else if (scrollTop < lastScrollTop - 5) {
+  } else if (scrollTop < lastScrollTop - 5) {
     rectangulo.classList.remove('oculto');
   }
 

4bb4103d5474559b4cf1cb6655bcb937eafce68d
juligric <49377944@est.ort.edu.ar>
2025-07-16 15:23:12 -0300

    ECHO EL BOTON

---
+++ pestaña.js
@@ -0,0 +1,14 @@
+let lastScrollTop = window.pageYOffset || document.documentElement.scrollTop;
+const rectangulo = document.querySelector('.rectangulo');
+
+window.addEventListener('scroll', function () {
+  const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+  if (scrollTop > lastScrollTop + 5) {
+    rectangulo.classList.add('oculto');
+  } else if (scrollTop < lastScrollTop - 5) {
+    rectangulo.classList.remove('oculto');
+  }
+
+  lastScrollTop = scrollTop;
+});
